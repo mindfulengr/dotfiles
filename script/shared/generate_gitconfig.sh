7b93108bb7f92000ca2b3ad9c7b27691bf5c7475
Paul Werner <mindfulengr@gmail.com>
2019-12-18 19:24:22 +0100

    fix typos during gitconfig generation

--- script/shared/generate_gitconfig.sh
+++ script/shared/generate_gitconfig.sh
@@ -10,9 +10,9 @@ generate_gitconfig() {
         user ' - What is your github author name?'
         read -e git_authorname
         user ' - What is your github author email?'
-        read -e git_authoreemail
+        read -e git_authoremail
 
-        sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.template > git/gitconfig.symlink}
+        sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.template > git/gitconfig.symlink
 
         success 'generated gitconfig'
     else
